413afc7419c5c505ec67ded2b011c1b35a38053f
Akash Prajapati <prajapatiaakash816@gmail.com>
2026-04-03 20:53:46 +0530

    Booking Module Updated

--- routes/Admin/bookings.js
+++ routes/Admin/bookings.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getBookingCounts, getCardsBookingsData } from '../../controllers/admin/bookingController.js';
+import { getBookingMetrics, getBookings } from '../../controllers/admin/bookingController.js';
 import { authAdmin, authorizeAdmin } from '../../middleware/authAdminMiddleware.js';
 const router = express.Router();
 
-router.get("/bookings/counts", authAdmin, authorizeAdmin, getBookingCounts);
+router.get("/bookings/counts", authAdmin, authorizeAdmin, getBookingMetrics);
 
-router.get("/bookings", authAdmin, authorizeAdmin, getCardsBookingsData);
+router.get("/bookings", authAdmin, authorizeAdmin, getBookings);
 
 export default router;
